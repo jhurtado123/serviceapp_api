ba74826dfbfb37427c1001fd9d2875ce090e1109
Jose Hurtado <hurtadojose89@gmail.com>
2020-05-06 20:15:01 +0200

    [dev] - allow cors request

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const indexRouter = require('./routes/api/index');
 const usersRouter = require('./routes/api/users');
 
