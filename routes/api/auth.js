1063e03245d71c9d455642e1dd56bb188aa7a158
Jose Hurtado <hurtadojose89@gmail.com>
2020-05-07 19:16:06 +0200

    [dev] - recover async await

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,67 +5,56 @@ const bcrypt = require("bcrypt");
 const User = require("../../models/User");
 
 router.get('/whoami', (req, res, next) => {
-  if(req.session.currentUser) {
+  if (req.session.currentUser) {
     res.status(200).json(req.session.currentUser)
   } else {
-    res.status(401).json({data:'not-authorized'})
+    res.status(401).json({data: 'not-authorized'})
   }
 });
 
 router.post('/signup', async (req, res, next) => {
-  const { username, password } = req.body;
-  console.log(username);
+  const {username, password} = req.body;
   try {
-    User.findOne({ username})
-      .then(user => {
-        if (user) {
-          return res.status(422).json({ data: 'El nombre de usuario ya existe'})
-        }
-        const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(password, salt);
+    const user = await User.findOne({username});
+    if (user) {
+      return res.status(422).json({data: 'El nombre de usuario ya existe'})
+    }
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
 
-        return User.create({username, password: hash})
-      })
-      .then(newUser => {
-        req.session.currentUser = newUser;
-        return res.status(200).json(newUser);
-      })
-     .catch(error => {
-       return res.status(500).json({data: 'Server error'});
-     })
-
+    const newUser = await User.create({username, password: hash});
+    if (!newUser) {
+      throw new Error();
+    }
+    req.session.currentUser = newUser;
+    return res.status(200).json(newUser);
 
   } catch (error) {
-    next(error)
+    return res.status(500).json({data: 'Server error'});
   }
 });
 
-router.post('/login', (req, res, next) => {
-  const { username, password } = req.body;
+router.post('/login', async (req, res, next) => {
+  const {username, password} = req.body;
   try {
-    User.findOne({username})
-      .then(user => {
-        if(!user) {
-          return res.status(404).json({ data: 'Nombre de usuario o contraseña incorrectos.'});
-        }
-        if (bcrypt.compareSync(password, user.password)) {
-          req.session.currentUser = user;
-          return res.status(200).json(user);
-        } else {
-          return res.status(404).json({ data: 'Nombre de usuario o contraseña incorrectos.'});
-        }
-      })
-      .catch(error => {
-        return res.status(404).json({ data: "user-not-found" });
-      });
-  } catch (error){ 
-    next(error)
+    const user = await User.findOne({username});
+    if (!user) {
+      return res.status(404).json({data: 'Nombre de usuario o contraseña incorrectos.'});
+    }
+    if (bcrypt.compareSync(password, user.password)) {
+      req.session.currentUser = user;
+      return res.status(200).json(user);
+    } else {
+      return res.status(404).json({data: 'Nombre de usuario o contraseña incorrectos.'});
+    }
+  } catch (error) {
+    return res.status(500).json({data: "Server error"});
   }
 });
 
 router.get('/logout', (req, res, next) => {
-  req.session.destroy(error=>{
-    if(error) {
+  req.session.destroy(error => {
+    if (error) {
       next(error)
     }
     return res.status(204).send()
