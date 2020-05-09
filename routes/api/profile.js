1dc7093c28394229e645217fd3ee1b9658f3649c
Jaime <jaimemoravallejo@gmail.com>
2020-05-09 14:26:29 +0200

    Create and get Level

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../../models/User');
+const Level = require('../../models/Level');
 
 /* GET user */
 router.get('/', (req, res, next) => {
@@ -13,5 +14,22 @@ router.get('/', (req, res, next) => {
     return res.status(500).json({ data: 'Server error' });
     })
 });
+
+router.get('/level', async (req, res, next) => {
+    const { currentUser } = req.session;
+    const userpoints = await User.findById(currentUser._id)
+    .then((user) => {
+      return user.points;
+    })
+    .catch((error) => console.log(error))
+    console.log("Los puntos", userpoints)
+    const level = await Level.find({ "maxpoints": {$gte: userpoints}, "minpoints": {$lte: userpoints}})
+    .then((level) => {
+      console.log(level, userpoints)
+      return res.status(200).json(level)
+    })
+    .catch((error) => {console.log(error)})
+    
+})
 
 module.exports = router;
