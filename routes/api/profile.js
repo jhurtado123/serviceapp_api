82350c6d54cf1f4cfc7757d930365d49b88cc215
Jaime <jaimemoravallejo@gmail.com>
2020-05-10 22:47:32 +0200

    WIP

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const path = require("path");
+const multer = require("multer");
+const fs = require("fs");
 const User = require('../../models/User');
 const Ad = require('../../models/Ad');
 const Level = require('../../models/Level');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
 
 /* GET user */
 router.get('/', (req, res, next) => {
@@ -30,6 +44,37 @@ router.get('/level', async (req, res, next) => {
     .catch((error) => {console.log(error)})
     
 })
+
+router.put("/edit", upload.any(), async (req, res, next) => {
+  const { currentUser } = req.session;
+  const files = req.files;
+  let { name, description, address, number, postalcode, lat, lng } = req.body;
+  const images = [];
+  files.forEach((file) => images.push(file.filename));
+
+  User.findOneAndUpdate(
+    { '_id': currentUser._id },
+    { name, description, address, number, postalcode, lat, lng, 'profile_image': images[0] }
+    )
+    .then((user) => {
+
+      const profileDirectory = `./public/uploads/profile/${user._id}`;
+      files.forEach((file) => {
+        try {
+          fs.readdirSync(profileDirectory).forEach((file) => {
+            fs.unlinkSync(`${profileDirectory}/${file}`);
+          });
+        } catch (e) {}
+        fs.rename(file.path, `${profileDirectory}/${file.filename}`, function (err) {
+          if (err) next();
+        })
+      });
+      return res.status(200).json({data: true});
+    })
+    .catch((error) => {
+      return next();
+    });
+  });
 
 router.get('/ads', (req, res,next) => {
   const user = req.session.currentUser;
