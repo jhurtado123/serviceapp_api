9f648f68f127f52edf859a3f73fb1dc50bcdf455
Jose Hurtado <hurtadojose89@gmail.com>
2020-05-17 20:52:43 +0200

    [56] -appointment

--- routes/api/appointment.js
+++ routes/api/appointment.js
@@ -2,14 +2,27 @@ const express = require("express");
 const router = express.Router();
 const autMiddleware = require('../../middlewares/authMiddleware');
 const Appointment = require('../../models/Appointment');
+const User = require('../../models/User');
 
 
 router.get('/:id', autMiddleware.checkIfLoggedIn, async (req, res, next) => {
   const {currentUser} = req.session;
   const {id} = req.params;
   try {
-    const appointment =  await Appointment.find({_id: id, status: 'active', $or: [{seller: currentUser}, {buyer: currentUser}]}).populate('ad seller buyer');
+    const appointment =  await Appointment.findOne({_id: id, status: 'active', $or: [{seller: currentUser}, {buyer: currentUser}]}).populate('ad seller buyer chat');
     return res.status(200).json({appointment});
+  } catch (e) {
+    return next(e);
+  }
+});
+
+router.delete('/:id', autMiddleware.checkIfLoggedIn, async (req, res, next) => {
+  const {currentUser} = req.session;
+  const {id} = req.params;
+  try {
+    const appointment =  await Appointment.findOneAndUpdate({_id: id, status: 'active', $or: [{seller: currentUser}, {buyer: currentUser}]}, {status: 'canceled'});
+    await User.findOneAndUpdate({_id: appointment.buyer}, {$inc : {'wallet.tokens':  appointment.pendingTokens }});
+    return res.status(200).json({data: true});
   } catch (e) {
     return next(e);
   }
