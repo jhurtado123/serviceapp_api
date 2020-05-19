48835942b974a25f726e397162510bb66ab8c8b8
Jose Hurtado <hurtadojose89@gmail.com>
2020-05-19 23:11:56 +0200

    [54] - socket emit to custom socket

--- middlewares/notificationMiddleware.js
+++ middlewares/notificationMiddleware.js
@@ -1,20 +1,25 @@
 const User = require('../models/User');
 const socket = require('../lib/socketIo');
 
-module.exports = async function createNofifications (id, options) {
-  try{
-    const user = await User.findOne({ '_id': id })
-        user.notifications.push({ 'title': options.title, 'href': options.href });
-        user.save();
-        console.log("ENTRAAAAA", socket.io.sockets.connected);
-        console.log("LA ID", id)
-    socket.io.sockets.connected[id].emit('notification:count', {
-      value: 1
+module.exports = async function createNofifications(id, options) {
+  try {
+    const user = await User.findOne({'_id': id})
+    user.notifications.push({'title': options.title, 'href': options.href});
+    user.save();
+
+    Object.keys(socket.io.sockets.connected).forEach((socketId) => {
+      const socketCustomID = socket.io.sockets.connected[socketId].id;
+      if (socketCustomID === id) {
+        socket.io.to(socketId).emit('notification:count', {
+          //Todo
+          value: 1
+        });
+      }
     });
 
-        return true;
-  }
-  catch(err) {
+
+    return true;
+  } catch (err) {
     console.log(err);
   }
 };
